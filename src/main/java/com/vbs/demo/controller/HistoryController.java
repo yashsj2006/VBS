2d9bb1ff8d436c3689b9661a86037d800d4182f1
yashsj2006 <yashsj2006@gmail.com>
2025-12-30 11:41:03 +0530

    Initial Commit

---
+++ src/main/java/com/vbs/demo/controller/HistoryController.java
@@ -0,0 +1,23 @@
+package com.vbs.demo.controller;
+
+import com.vbs.demo.models.History;
+import com.vbs.demo.repositories.HistoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HistoryController {
+    @Autowired
+    HistoryRepo historyRepo;
+    @GetMapping("/histories")
+        public List<History> getUserHistory()
+    {
+        return historyRepo.findAll();
+    }
+}
